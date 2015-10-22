f5dd8b9aa7d493236cea5a9baa4da7df8f098e56
carlos <carlos.ortega@strath.ac.uk>
2015-10-22 11:23:14 +0100

    Added evaluate(x) methods for Chebyshev Polynomials

--- src/main_tests.cpp
+++ src/main_tests.cpp
@@ -2,18 +2,14 @@
 
 void main_tests(){
 
-    int nvar=3;
+    int nvar=1;
     int degree=100;
     Chebyshev_Polynomial<double> x (nvar, degree,0);
-    Chebyshev_Polynomial<double> y (nvar, degree,1);
-    Chebyshev_Polynomial<double> z (nvar, degree,2);
 
-    Chebyshev_Polynomial<double> p = 3*x+y+z;
+    Chebyshev_Polynomial<double> p = (1+0.5*x+.3*x*x+4.6*x*x*x+18*x*x*x*x)/(x+2);
 
     std::vector<double> point;
     point.push_back(.6);
-    point.push_back(-1);
-    point.push_back(.44);
 
     cout << p << endl << endl;
     cout << p.evaluate(point) << endl;
